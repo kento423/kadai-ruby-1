a4cb66ccf62bca91d4b560653d57f7608093156c
kento.okamoto <mntymnty6@gmail.com>
2022-10-11 03:31:55 +0000

    レビュー指摘修正

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -2,5 +2,5 @@ last_name = '岡本'
 first_name = '健人'
 age = 28
 
-# puts last_name + first_name + "です。" + age.to_s + "歳です。"
-puts "#{last_name}#{first_name}です。#{age}歳です。"
+puts last_name + first_name + "です。" + age.to_s + "歳です。"
+# puts "#{last_name}#{first_name}です。#{age}歳です。"
